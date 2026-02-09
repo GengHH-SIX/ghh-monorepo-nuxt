3c8c5257d9a9401f79447c207b739fd6cea965d1
GengHH <18818060415@163.com>
2026-02-10 01:05:12 +0800

    优化浏览器模式测试配置文件，在浏览器环境中添加vue模版编译器

--- packages/my-motion/vitest.browser.config.ts
+++ packages/my-motion/vitest.browser.config.ts
@@ -4,8 +4,17 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
 	plugins: [vue()],
+	resolve: {
+		alias: {
+			'vue': 'vue/dist/vue.esm-bundler.js'
+		}
+	},
 	test: {
 		globals: true,
+		include: [
+			'src/**/*.{test,spec}.{js,ts}',
+			'src/**/__test__/**/*.{test,spec}.{js,ts}'
+		],
 		// Vitest 浏览器模式 (组件测试)
 		browser: {
 			enabled: true,
