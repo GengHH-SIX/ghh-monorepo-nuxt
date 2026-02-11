5b39e759655d52802953acf005b807c65a0d673a
GengHH <18818060415@163.com>
2026-02-11 15:16:38 +0800

    修改组件重复导出的问题

--- packages/my-motion/src/components/index.ts
+++ packages/my-motion/src/components/index.ts
@@ -2,5 +2,3 @@ export type { RootProps } from './AnimationRoot';
 export { default as AnimationRoot } from './AnimationRoot';
 export type { UpMotionProps } from './UpMotion';
 export { default as UpMotion } from './UpMotion';
-export type { UpMotionProps } from './UpMotion';
-export { default as UpMotion } from './UpMotion';
